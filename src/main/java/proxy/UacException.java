4e757c648f189a59fc012d7c7d6ad4fb6edc486e
alexwais <alex.wais@me.com>
2013-12-29 18:45:01 +0100

    java1.6 compliance

--- src/main/java/proxy/UacException.java
+++ src/main/java/proxy/UacException.java
@@ -25,10 +25,4 @@ public class UacException extends Exception {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
 	}
-
-	public UacException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
-
 }
